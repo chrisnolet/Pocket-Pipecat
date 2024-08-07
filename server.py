4a4b4ace5de053810af8089ed9200308a7210ffb
Chris Nolet <chris@relaunch.io>
2024-08-06 19:06:33 -0700

    Add Dockerfile

--- server.py
+++ server.py
@@ -126,7 +126,7 @@ def get_status(pid: int):
 if __name__ == "__main__":
     import uvicorn
 
-    default_host = os.getenv("HOST", "127.0.0.1")
+    default_host = os.getenv("HOST", "0.0.0.0")
     default_port = int(os.getenv("PORT", "8000"))
 
     parser = argparse.ArgumentParser(description="Daily Storyteller FastAPI server")
